328707c27741aeb6f70bef0b665748c44af98581
varshajnagaraja <varsha.nagaraja@block.one>
2019-03-18 18:28:52 +0800

    api-rpc skeleton with eosjs

---
+++ packages/api-rpc/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var get_info_1 = __importDefault(require("./api/get_info"));
+exports.default = {
+    get_info: get_info_1.default
+};
