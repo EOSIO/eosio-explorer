2771141480655591825c67b5909ab64a917197bb
Jonathan Cardenas <jonathan.cardenas430@gmail.com>
2019-03-18 18:20:40 +0800

    [20190318] Improve file handling algorithm for source folder of .cpp and .hpp files to include directories in the eosio-cpp command which contain .cpp and .hpp files aside from the source folder. Assuming that all subdirectories are in the same root folder.

--- packages/api-eosio-compiler/docker-eosio-cdt/quick_start.sh
+++ packages/api-eosio-compiler/docker-eosio-cdt/quick_start.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+CONTRACTFILE=$(basename -- "$1")
+EXTENSION="${CONTRACTFILE##*.}"
+CONTRACTNAME="${CONTRACTFILE%.*}"
+FULLFILEPATH=$1
+
 # Verify everything is clean and setup properly
 echo "[Quick Start Engage] First time setup..."
 ./first_time_setup.sh
@@ -28,14 +33,16 @@ if [ ! "$(docker ps -q -f name=eosio_gui_nodeos_cdt_container)" ]; then
     ./start_eosio_cdt_docker.sh
 
     echo ">> Attempt compilation of smart contract file"
-    docker exec -i eosio_gui_nodeos_cdt_container ./scripts/compile_contract.sh src/eosiotoken.cpp \
+    shift 1
+    echo "$@"
+    docker exec -i eosio_gui_nodeos_cdt_container ./scripts/compile_contract.sh "$FULLFILEPATH" "$@" \
         &> log.txt \
         && echo "exec pass" || echo "exec fail"
 
     echo ">> See log.txt for piped file..."
 
     echo ">> Attempt copying of smart contract..."
-    docker cp eosio_gui_nodeos_cdt_container:/opt/eosio/bin/compiled_contracts/eosiotoken "$(pwd)"/compiled_contracts
+    docker cp eosio_gui_nodeos_cdt_container:/opt/eosio/bin/compiled_contracts/$CONTRACTNAME "$(pwd)"/compiled_contracts
     echo ">> docker cp operation finished"
 fi
 
--- packages/api-eosio-compiler/docker-eosio-cdt/scripts/compile_contract.sh
+++ packages/api-eosio-compiler/docker-eosio-cdt/scripts/compile_contract.sh
@@ -9,6 +9,7 @@ COMPILEDCONTRACTSPATH="$( pwd -P )/compiled_contracts"
 CONTRACTFILE=$(basename -- "$1")
 EXTENSION="${CONTRACTFILE##*.}"
 CONTRACTNAME="${CONTRACTFILE%.*}"
+FULLFILEPATH=$1
 
 # make new directory for compiled contract files
 # WASM and ABI
@@ -17,6 +18,15 @@ mkdir -p ./compiled_contracts
 mkdir -p ./compiled_contracts/$CONTRACTNAME
 
 # compile smart contract source files to WASM and ABI files using EOSIO.CDT
-(
-    eosio-cpp -abigen "$CONTRACTSPATH/$1" -o "$COMPILEDCONTRACTSPATH/$CONTRACTNAME/$CONTRACTNAME.wasm" -contract "$1" 
-) 
+if [ "$#" -eq 1 ]; then
+    (
+        eosio-cpp -abigen "$CONTRACTSPATH/$FULLFILEPATH" -o "$COMPILEDCONTRACTSPATH/$CONTRACTNAME/$CONTRACTNAME.wasm" -contract "$CONTRACTNAME" 
+    ) 
+fi
+
+if [ "$#" -ge 2 ]; then
+    shift 1
+    (
+        eosio-cpp -abigen "$CONTRACTSPATH/$FULLFILEPATH" -o "$COMPILEDCONTRACTSPATH/$CONTRACTNAME/$CONTRACTNAME.wasm" -contract "$CONTRACTNAME" -I=$@ -w
+    ) 
+fi
