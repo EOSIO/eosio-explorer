8ca4b14631b83857d157990d9dabbd4136db3724
ajay matharu <ajmatharu@gmail.com>
2019-03-19 13:38:33 +0800

    add permissionspage and createaccount page

---
+++ packages/ui-gui-nodeos/src/pages/PermissionsPage/PermissionsPage.jsx
@@ -0,0 +1,31 @@
+import './PermissionsPage.scss';
+
+import React, { Component } from 'react';
+import { Card, CardBody, CardHeader, Col } from 'reactstrap';
+
+import { StandardTemplate } from 'templates';
+import CreateAccount from './components/CreateAccount';
+
+
+class PermissionsPage extends Component {
+
+  render() {
+
+    return (
+      <StandardTemplate>
+        <div className="PermissionsPage">
+          <Card>
+            <CardHeader>
+              Permissions Page
+            </CardHeader>
+            <CardBody>
+              <CreateAccount/>
+            </CardBody>
+          </Card>
+        </div>
+      </StandardTemplate>
+    );
+  }
+}
+
+export default PermissionsPage;
---
+++ packages/ui-gui-nodeos/src/pages/PermissionsPage/components/CreateAccount/CreateAccount.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const CreateAccount = (props) => {
+
+  return (
+    <div className="CreateAccount">
+        this is create account page
+    </div>
+  );
+}
+
+export default CreateAccount;
