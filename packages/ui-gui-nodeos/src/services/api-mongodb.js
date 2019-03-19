68056cfcf82d2f76aad4365da303336fb8e9d20e
Terry Leung <kingsangterry@gmail.com>
2019-03-19 10:10:09 +0800

    Return a dummy promise while making async calls while prerendering ( running react-snap ) to prevent timeout / unwanted behaviour.

--- packages/ui-gui-nodeos/src/services/api-mongodb.js
+++ packages/ui-gui-nodeos/src/services/api-mongodb.js
@@ -1,8 +1,14 @@
+import { from } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
-export default ( endpoint ) =>
+export default ( endpoint ) => navigator.userAgent !== "ReactSnap"
+?
   ajax({
     url :`/api/mongodb/${endpoint}`,
     timeout: 1000,
     responseType: "json"
   })
+: //If we are prerending using react snap, return a promise that resolved with a repsonse with { repsonse: null}
+  from(new Promise((resolve, reject)=>{
+    resolve({response: null});
+  }))
