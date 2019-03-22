e5a0bbb5331d7c37e5d3871d2831901f1bd1cc64
ajay matharu <ajmatharu@gmail.com>
2019-03-22 14:21:18 +0800

    renamed get_block to get_block_details

--- packages/api-mongodb-plugin/src/api/index.ts
+++ packages/api-mongodb-plugin/src/api/index.ts
@@ -2,7 +2,7 @@
 import get_accounts from './get_accounts';
 import get_block_latest from './get_block_latest';
 import get_blocks from './get_blocks';
-import get_block from './get_block';
+import get_block_details from './get_block_details';
 import get_transactions from './get_transactions';
 import get_actions from './get_actions';
 import get_all_permissions from './get_all_permissions';
@@ -11,7 +11,7 @@ export default {
   get_accounts,
   get_block_latest,
   get_blocks,
-  get_block,
+  get_block_details,
   get_transactions,
   get_actions,
   get_all_permissions
