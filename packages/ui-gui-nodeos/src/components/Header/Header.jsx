c836244d418c5fd8dba57b9d184fc6349930cd18
Matt <mkelly2@live.co.uk>
2019-03-22 10:32:12 +0800

    Added basic Info page UI skeleton

--- packages/ui-gui-nodeos/src/components/Header/Header.jsx
+++ packages/ui-gui-nodeos/src/components/Header/Header.jsx
@@ -12,7 +12,6 @@ const Header = () =>
     <>
       <AppNavbarBrand
           full={{ src: null, width: 89, height: 25, alt: 'Nodeos GUI' }}
-          minimized={{ src: null, width: 30, height: 30, alt: 'Nodeos GUI' }}
         />
       <Nav className="d-md-down-none" navbar>
         <NavItem className="px-3">
--- packages/ui-gui-nodeos/src/pages/InfoPage/InfoPage.jsx
+++ packages/ui-gui-nodeos/src/pages/InfoPage/InfoPage.jsx
@@ -3,7 +3,7 @@ import './InfoPage.scss';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Button, Card, CardBody, CardHeader, Col } from 'reactstrap';
+import { Button, Card, CardBody, CardHeader, Col, Row, InputGroup, InputGroupAddon, Form, FormGroup, Input, Label } from 'reactstrap';
 
 import { StandardTemplate } from 'templates';
 import { increaseCounter } from 'actions/counter';
@@ -38,24 +38,203 @@ class InfoPage extends Component {
     return (
       <StandardTemplate>
         <div className="InfoPage animated fadeIn">
-            <Card>
-              <CardHeader>
-                Landing Page Content
-              </CardHeader>
-              <CardBody>
-                <p><strong>Calls from mongodb:</strong> {text}</p>
-                <p><strong>Blockchain info from RPC API: </strong> {info}</p>
-                <p><strong>Counter:</strong> {this.props.count}</p>
-                <Col col="4" sm="8" md="4" className="mb-3 text-center">
-                  <Button block color="primary" onClick={()=>{this.props.updateCounter(2)}}>Click here to increase counter by 2</Button>
-                </Col>
-                <Headblock/>
-                <p>Scroll down</p>
-                <p>
-                  <Link to={'/some'}>Go to Some Page</Link>
-                </p>
-              </CardBody>
-            </Card>
+          <Row>
+            <Col xs="12">
+              <h2 className="pageTitle text-center mb-4">Info Page</h2>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs="6">
+              <Card>
+                <CardHeader>
+                  Nodeos
+                </CardHeader>
+                <CardBody>
+                  <Form action="" method="post" className="form-horizontal">
+                    <FormGroup row className="mb-0">
+                      <Col xs="4">
+                        <Label htmlFor="nodeosEndPoint">Connected Nodeos</Label>
+                      </Col>
+                      <Col xs="8">
+                        <InputGroup>
+                          <Input type="email" id="nodeosEndPoint" name="nodeosEndPoint" placeholder="Enter Endpoint..." />
+                          <InputGroupAddon addonType="append">
+                            <Button type="button" color="primary">Connect</Button>
+                          </InputGroupAddon>
+                        </InputGroup>
+                      </Col>
+                    </FormGroup>
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+
+            <Col xs="6">
+              <Card>
+                <CardHeader>
+                  Mongodb
+                </CardHeader>
+                <CardBody>
+                  <Form action="" method="post" className="form-horizontal">
+                    <FormGroup row className="mb-0">
+                      <Col xs="4">
+                        <Label htmlFor="mongodbEndPoint">Connected Mongodb</Label>
+                      </Col>
+                      <Col xs="8">
+                        <InputGroup>
+                          <Input type="email" id="mongodbEndPoint" name="mongodbEndPoint" placeholder="Enter Endpoint..." />
+                          <InputGroupAddon addonType="append">
+                            <Button type="button" color="primary">Connect</Button>
+                          </InputGroupAddon>
+                        </InputGroup>
+                      </Col>
+                    </FormGroup>
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs="12">
+              <Card>
+                <CardHeader>
+                  Block Information
+                </CardHeader>
+                <CardBody>
+
+                  <Form className="form-horizontal">
+                    <FormGroup row className="mb-0">
+                      <Col xs="2">
+                        <Label><strong>Server Version</strong></Label>
+                      </Col>
+                      <Col xs="10">
+                        <p className="form-control-static">9cc225b8</p>
+                      </Col>
+                    </FormGroup>
+                    <FormGroup row className="mb-0">
+                      <Col xs="2">
+                        <Label><strong>Server Version String</strong></Label>
+                      </Col>
+                      <Col xs="10">
+                        <p className="form-control-static">mainnet-1.6.0</p>
+                      </Col>
+                    </FormGroup>
+                    <FormGroup row className="mb-0">
+                      <Col xs="2">
+                        <Label><strong>Chain ID</strong></Label>
+                      </Col>
+                      <Col xs="10">
+                        <p className="form-control-static">aca376f206b8fc25a6ed44</p>
+                      </Col>
+                    </FormGroup>
+                  </Form>
+
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs="12">
+              <Card>
+                <CardHeader>
+                  Head Block Information
+                </CardHeader>
+                <CardBody>
+                  <Form className="form-horizontal">
+                    <FormGroup row className="mb-0">
+                      <Col xs="2">
+                        <Label><strong>Block Number</strong></Label>
+                      </Col>
+                      <Col xs="10">
+                        <p className="form-control-static">4261597</p>
+                      </Col>
+                    </FormGroup>
+                    <FormGroup row className="mb-0">
+                      <Col xs="2">
+                        <Label><strong>Block ID</strong></Label>
+                      </Col>
+                      <Col xs="10">
+                        <p className="form-control-static">0000d86d5dba32b2ead57692f03fce45403331340451e296dae7295c180c3064</p>
+                      </Col>
+                    </FormGroup>
+                    <FormGroup row className="mb-0">
+                      <Col xs="2">
+                        <Label><strong>Timestamp</strong></Label>
+                      </Col>
+                      <Col xs="10">
+                        <p className="form-control-static">2019-03-19T10:32:10.000</p>
+                      </Col>
+                    </FormGroup>
+                    <FormGroup row className="mb-0">
+                      <Col xs="2">
+                        <Label><strong>Block Producer</strong></Label>
+                      </Col>
+                      <Col xs="10">
+                        <p className="form-control-static">eosio</p>
+                      </Col>
+                    </FormGroup>
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs="12">
+              <Card>
+                <CardHeader>
+                  Last Irreversible Block Information
+                </CardHeader>
+                <CardBody>
+                  <Form className="form-horizontal">
+                    <FormGroup row className="mb-0">
+                      <Col xs="2">
+                        <Label><strong>Block Number</strong></Label>
+                      </Col>
+                      <Col xs="10">
+                        <p className="form-control-static">42671296</p>
+                      </Col>
+                    </FormGroup>
+                    <FormGroup row className="mb-0">
+                      <Col xs="2">
+                        <Label><strong>Block ID</strong></Label>
+                      </Col>
+                      <Col xs="10">
+                        <p className="form-control-static">0000d86d5dba32b2ead57692f03fce45403331340451e296dae7295c180c3064</p>
+                      </Col>
+                    </FormGroup>
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs="12">
+              <Card>
+                <CardHeader>
+                  Landing Page Content
+                </CardHeader>
+                <CardBody>
+                  <p><strong>Calls from mongodb:</strong> {text}</p>
+                  <p><strong>Blockchain info from RPC API: </strong> {info}</p>
+                  <p><strong>Counter:</strong> {this.props.count}</p>
+                  <Col col="4" sm="8" md="4" className="mb-3 text-center">
+                    <Button block color="primary" onClick={()=>{this.props.updateCounter(2)}}>Click here to increase counter by 2</Button>
+                  </Col>
+                  <Headblock/>
+                  <p>Scroll down</p>
+                  <p>
+                    <Link to={'/some'}>Go to Some Page</Link>
+                  </p>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+
         </div>
       </StandardTemplate>
     );
