ce5ed99ac9f97bb0884865c80f48be17997a594f
ajay matharu <ajmatharu@gmail.com>
2019-03-22 17:23:30 +0800

    - renamed filter to show_empty - added search by id_or_num functionality - only fetch required columns

--- packages/api-mongodb-plugin/src/api/get_blocks.ts
+++ packages/api-mongodb-plugin/src/api/get_blocks.ts
@@ -1,13 +1,30 @@
 import BlockModel from '../models/block';
+import { Int32 } from 'bson';
 
 export default async (query:any) => {
   try{
-    let { filter } = query;
+    let { show_empty, id_or_num } = query;
     let result: Object;
-    
-    filter !== 'true' 
-      ? result = await BlockModel.find({}, {}, { sort: { 'createdAt' : -1 } }).limit(100)
-      : result = await BlockModel.find({"block.transactions": { $ne: [] }}, {}, { sort: { 'createdAt' : -1 } }).limit(100);  
+
+    let query_gen = BlockModel.find({},
+          {
+            "block_id": 1,
+            "block_num": 1,
+            "createdAt": 1,
+            "block.transactions.trx.id": 1
+          });
+
+    (show_empty === undefined || show_empty !== 'true')?
+      query_gen.exists("block.transactions.status"): "";
+
+    // check if id is passed
+    // check if its a number or not else it gives parsing error
+    (id_or_num !== undefined) ? isNaN(Number(id_or_num)) ? 
+        query_gen.where({block_id: id_or_num}): query_gen.where({block_num: id_or_num}) : "";
+
+    query_gen.limit(100);
+    query_gen.sort({createdDate: -1});
+    result = await query_gen.exec();
     return result;
   }catch(err){
     console.log(err);
