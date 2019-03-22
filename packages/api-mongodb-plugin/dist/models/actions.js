aefbed44e59c73cc6b78c727867a200588be8729
ajay matharu <ajmatharu@gmail.com>
2019-03-22 11:47:01 +0800

    add get-actions api call

---
+++ packages/api-mongodb-plugin/dist/models/actions.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongoose_1 = __importDefault(require("mongoose"));
+var ActionsSchema = new mongoose_1.default.Schema({
+    _id: {
+        type: String,
+        required: true
+    }
+});
+exports.default = mongoose_1.default.model('Actions', ActionsSchema, 'action_traces');
--- packages/api-mongodb-plugin/src/api/index.ts
+++ packages/api-mongodb-plugin/src/api/index.ts
@@ -4,11 +4,14 @@ import get_block_latest from './get_block_latest';
 import get_blocks from './get_blocks';
 import get_block from './get_block';
 import get_transactions from './get_transactions';
+import get_actions from './get_actions';
+
 
 export default {
   get_accounts,
   get_block_latest,
   get_blocks,
   get_block,
-  get_transactions
+  get_transactions,
+  get_actions
 };
