fa3747781b702529207c7bf3e42e01bf05d64a8d
ajay matharu <ajmatharu@gmail.com>
2019-03-25 16:21:51 +0800

    added create_account rpc api

--- packages/api-rpc/dist/index.js
+++ packages/api-rpc/dist/index.js
@@ -4,6 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var get_info_1 = __importDefault(require("./api/get_info"));
+var create_account_1 = __importDefault(require("./api/create_account"));
 exports.default = {
-    get_info: get_info_1.default
+    get_info: get_info_1.default,
+    create_account: create_account_1.default
 };
