fb8e6354cb17aa1100fed19b051b924861269748
ajay matharu <ajmatharu@gmail.com>
2019-03-25 18:32:35 +0800

    added get_table_rows rpc api call

--- packages/api-rpc/dist/index.js
+++ packages/api-rpc/dist/index.js
@@ -6,8 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var get_info_1 = __importDefault(require("./api/get_info"));
 var create_account_1 = __importDefault(require("./api/create_account"));
 var push_action_1 = __importDefault(require("./api/push_action"));
+var get_table_rows_1 = __importDefault(require("./api/get_table_rows"));
 exports.default = {
     get_info: get_info_1.default,
     create_account: create_account_1.default,
-    push_action: push_action_1.default
+    push_action: push_action_1.default,
+    get_table_rows: get_table_rows_1.default
 };
