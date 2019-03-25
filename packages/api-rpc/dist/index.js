846f5328061f48c261b1b1dfb7698f47d352ddfa
ajay matharu <ajmatharu@gmail.com>
2019-03-25 17:07:55 +0800

    add push action rpc api

--- packages/api-rpc/dist/index.js
+++ packages/api-rpc/dist/index.js
@@ -5,7 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var get_info_1 = __importDefault(require("./api/get_info"));
 var create_account_1 = __importDefault(require("./api/create_account"));
+var push_action_1 = __importDefault(require("./api/push_action"));
 exports.default = {
     get_info: get_info_1.default,
-    create_account: create_account_1.default
+    create_account: create_account_1.default,
+    push_action: push_action_1.default
 };
