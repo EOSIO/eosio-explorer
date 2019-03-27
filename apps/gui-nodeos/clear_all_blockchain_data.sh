c6995b47dce85b3754c6b0016758c086ada12929
Jonathan J. Cardenas <jonathan.cardenas@HK000001.local>
2019-03-27 15:42:58 +0800

    [20190327] minor fix to first_time_setup script in docker-eosio-nodeos

--- apps/gui-nodeos/clear_all_blockchain_data.sh
+++ apps/gui-nodeos/clear_all_blockchain_data.sh
@@ -4,8 +4,13 @@ set -o errexit
 EXECPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOTPATH="../..";
 
-echo "=== stopping the nodeos container... ==="
-docker stop eosio_gui_nodeos_container
+echo "=== Checking if nodeos container is running... ==="
+if [ "$(docker ps -q -f name=eosio_gui_nodeos_container)" ]; then
+    if [ "$(docker ps -aq -f status=running -f name=eosio_gui_nodeos_container)" ]; then
+        echo "=== Blockchain container is running, stopping the operation... ==="
+        docker stop eosio_gui_nodeos_container
+    fi
+fi
 
 echo "=== reinitializing nodeos with cleared blockchain ==="
 (cd ${ROOTPATH}/packages/ && exec docker-eosio-nodeos/first_time_setup.sh)
