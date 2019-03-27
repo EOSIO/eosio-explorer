9b84b7ec974135adb8bd11f3cd30db86db1bb790
ajay matharu <ajmatharu@gmail.com>
2019-03-27 14:54:40 +0800

    add code to deploy contract to service-logic.js

--- packages/api-eosio-compiler/service-logic.js
+++ packages/api-eosio-compiler/service-logic.js
@@ -8,6 +8,10 @@ const copy = require('recursive-copy');
 const del = require('del');
 const path = require('path');
 const Helper = require('./helpers');
+const { Api, JsonRpc, Serialize } = require('eosjs');
+const JsSignatureProvider = require('eosjs/dist/eosjs-jssig').default;
+const { TextEncoder, TextDecoder } = require('util'); 
+const fetch = require('node-fetch'); 
 
 const LOG_DEST = path.resolve("./docker-eosio-cdt/stdout.txt");
 const ERR_DEST = path.resolve("./docker-eosio-cdt/stderr.txt");
@@ -21,6 +25,9 @@ const SHUTDOWN_CMD = './remove_eosio_cdt_docker.sh';
  * localhost:8081/api/eosio/compile
  * POST request
  * 1. <source> - Absolute source file path
+ * 2. <endpoint> - Blockchain endpoint
+ * 3. <account_name> - account name on which the smart contract would be deployed
+ * 4. <private_key> - private key of account to sign the transaction
  */
 Router.post("/compile", async (req, res) => {
     const { body } = req;
@@ -28,7 +35,12 @@ Router.post("/compile", async (req, res) => {
     try {
         const deletedFiles = await del(DEL_TARGETS);
         let results = null;
+        console.log(typeof body["source"]);
         let compileTarget = path.basename(body["source"]);
+        console.log(compileTarget);
+        let endpoint = path.basename(body["endpoint"]);
+        let account_name = path.basename(body["account_name"]);
+        let private_key = path.basename(body["private_key"]);
         let COMPILE_SCRIPT = "";
 
         const directories = Helper.parseDirectoriesToInclude(path.dirname(body["source"]));
@@ -45,6 +57,7 @@ Router.post("/compile", async (req, res) => {
         exec(COMPILE_SCRIPT, {
             cwd: CWD
         }, (err, stdout, stderr) => {
+          console.log('compile script ran');
             execSync(SHUTDOWN_CMD, {
                 cwd: CWD
             })
@@ -72,17 +85,34 @@ Router.post("/compile", async (req, res) => {
                     })
                 } else {
                     console.log(`stdout: ${stdout}`);
-                    res.send({
+                    deployContract(endpoint, account_name, private_key, wasmPath, abiPath)
+                    .then(result => {
+                      console.log("Contract deployed successfully ", result);
+                      res.send({
                         compiled: true,
                         wasmLocation: wasmPath,
                         abi: abiPath,
+                        deployed: true,
+                        output: result,
                         stdout: parsedStdOut,
                         stderr: parsedStdErr
+                      });
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                      res.send({
+                        compiled: true,
+                        wasmLocation: wasmPath,
+                        abi: abiPath,
+                        deployed: false,
+                        errors: err,
+                        stdout: parsedStdOut,
+                        stderr: parsedStdErr
+                      });
                     });
                 }
             }
         });
-
     } catch (ex) {
         res.send({
             compiled: false,
@@ -93,5 +123,79 @@ Router.post("/compile", async (req, res) => {
         })
     }
 });
+
+async function deployContract(blockchainUrl, account_name, private_key, wasm_path, abi_path){
+  if(blockchainUrl.indexOf("http://") < 0)
+  {
+    blockchainUrl = "http://" + blockchainUrl;
+  }
+
+  const rpc = new JsonRpc(blockchainUrl, { fetch });
+  const signatureProvider = new JsSignatureProvider([private_key]);
+  const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+
+  const buffer = new Serialize.SerialBuffer({
+    textEncoder: api.textEncoder,
+    textDecoder: api.textDecoder,
+  });
+
+  const wasm = fs.readFileSync(wasm_path).toString('hex');
+  let abi = JSON.parse(fs.readFileSync(abi_path, 'utf8'));
+
+  const abiDefinition = api.abiTypes.get('abi_def');
+  // need to make sure abi has every field in abiDefinition.fields
+  // otherwise serialize throws error
+  abi = abiDefinition.fields.reduce(
+      (acc, { name: fieldName }) => Object.assign(acc, { [fieldName]: acc[fieldName] || [] }),
+      abi,
+  );
+  abiDefinition.serialize(buffer, abi);
+
+  try{
+    return await api.transact(
+      {
+        actions: [
+          {
+            account: 'eosio',
+            name: 'setcode',
+            authorization: [
+              {
+                actor: account_name,
+                permission: 'active',
+              },
+            ],
+            data: {
+              account: account_name,
+              vmtype: 0,
+              vmversion: 0,
+              code: wasm,
+            },
+          },
+          {
+            account: 'eosio',
+            name: 'setabi',
+            authorization: [
+              {
+                actor: account_name,
+                permission: 'active',
+              },
+            ],
+            data: {
+              account: account_name,
+              abi: Buffer.from(buffer.asUint8Array()).toString('hex'),
+            },
+          },
+        ],
+      },
+      {
+        blocksBehind: 3,
+        expireSeconds: 30,
+      }
+    );
+  }
+  catch(err){
+    throw("Error while deploying contract - " + err)
+  }
+}
 
 module.exports = Router;
