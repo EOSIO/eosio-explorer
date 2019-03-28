e9f450b0d46132829e75ff0d73638cce221c9236
varshajnagaraja <varsha.nagaraja@block.one>
2019-03-28 16:31:15 +0800

    Error handling in Accounts page

--- packages/ui-gui-nodeos/src/pages/AccountdetailPage/components/AccountDetail/Accountdetail.jsx
+++ packages/ui-gui-nodeos/src/pages/AccountdetailPage/components/AccountDetail/Accountdetail.jsx
@@ -57,101 +57,96 @@ const Accountdetail = (props) => {
 
   return (
     <div className="Accountdetail">
-      <div>{ error
-              ? <button onClick={props.fetchStart}>{JSON.stringify(error)} Click to Reload.</button>
-              : isFetching 
-                ? `loading...`
-                : <div>
+    <Row>
+      <Col sm="12">
+        <Card> 
+          <CardBody>                       
+            <SearchCardTitle>
+              <DivFlexStyled>
+                <SearchLabel>Search Account Name:</SearchLabel>
+                <SearchInputStyled 
+                      placeholder="Account Name"
+                      value={inputValue}
+                      onKeyDown={
+                        evt => {
+                          if (evt.key === 'Enter') {
+                            setInputValue("")
+                            { inputValue ? props.push('/account/'+inputValue) : console.log("No search text")} 
+                          }
+                        }
+                      }
+                      onChange={evt=>{setInputValue(evt.target.value)}}/>
+                <SearchButton
+                      color="secondary"                           
+                      onClick={evt=> {
+                        setInputValue("")
+                        {inputValue ? props.push('/account/'+inputValue) : console.log("No search text") }                          
+                      }}>
+                Search</SearchButton>
+              </DivFlexStyled>
+            </SearchCardTitle>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+      <div>
+        { showDetailsSection                     
+          ? error
+            ? `No account found with Account name: ${params.account_name}`
+            : isFetching
+              ? `loading...`
+              :  <div>
                     <Row>
                       <Col sm="12">
                         <Card> 
-                          <CardBody>                       
-                            <SearchCardTitle>
-                              <DivFlexStyled>
-                                <SearchLabel>Search Account Name:</SearchLabel>
-                                <SearchInputStyled 
-                                      placeholder="Account Name"
-                                      value={inputValue}
-                                      onKeyDown={
-                                        evt => {
-                                          if (evt.key === 'Enter') {
-                                            setInputValue("")
-                                            { inputValue ? props.push('/account/'+inputValue) : console.log("No search text")} 
-                                          }
-                                        }
-                                      }
-                                      onChange={evt=>{setInputValue(evt.target.value)}}/>
-                                <SearchButton
-                                      color="secondary"                           
-                                      onClick={evt=> {
-                                        setInputValue("")
-                                        {inputValue ? props.push('/account/'+inputValue) : console.log("No search text") }                          
-                                      }}>
-                                Search</SearchButton>
-                              </DivFlexStyled>
-                            </SearchCardTitle>
+                          <CardBody>  
+                            <CardTitleStyled>Account Detail</CardTitleStyled>
+                            <Form> 
+                              <FormGroup row>
+                                <ColBoldStyled sm={2}>Account Name:</ColBoldStyled>
+                                <Col sm={10}>
+                                  {payload.account_name}
+                                </Col>
+                              </FormGroup>
+                              <FormGroup row>
+                                <ColBoldStyled sm={2}>Account Creation Date:</ColBoldStyled>
+                                <Col sm={10}>
+                                  {payload.created}
+                                </Col>
+                              </FormGroup>
+                              <FormGroup row>
+                                <ColBoldStyled sm={2}>Owner Public Key:</ColBoldStyled>
+                                <Col sm={10}>                                            
+                                  {payload.permissions[0].perm_name == "owner" 
+                                    ? payload.permissions[0].required_auth.keys[0].key 
+                                    : payload.permissions[1].required_auth.keys[0].key}
+                                </Col>
+                              </FormGroup>
+                              <FormGroup row>
+                                <ColBoldStyled sm={2}>Active Public Key:</ColBoldStyled>
+                                <Col sm={10}>
+                                  {payload.permissions[0].perm_name == "active" 
+                                    ? payload.permissions[0].required_auth.keys[0].key 
+                                    : payload.permissions[1].required_auth.keys[0].key}
+                                </Col>
+                              </FormGroup>
+                            </Form>
                           </CardBody>
                         </Card>
                       </Col>
-                    </Row>                       
-                    {showDetailsSection                     
-                      ? error
-                        ? <button onClick={props.fetchStart}>{JSON.stringify(error)} Click to Reload.</button>
-                        : isFetching
-                          ? `loading...`
-                          :  <div>
-                                <Row>
-                                  <Col sm="12">
-                                    <Card> 
-                                      <CardBody>  
-                                        <CardTitleStyled>Account Detail</CardTitleStyled>
-                                        <Form> 
-                                          <FormGroup row>
-                                            <ColBoldStyled sm={2}>Account Name:</ColBoldStyled>
-                                            <Col sm={10}>
-                                              {payload.account_name}
-                                            </Col>
-                                          </FormGroup>
-                                          <FormGroup row>
-                                            <ColBoldStyled sm={2}>Account Creation Date:</ColBoldStyled>
-                                            <Col sm={10}>
-                                              {payload.created}
-                                            </Col>
-                                          </FormGroup>
-                                          <FormGroup row>
-                                            <ColBoldStyled sm={2}>Owner Public Key:</ColBoldStyled>
-                                            <Col sm={10}>                                            
-                                              {payload.permissions[0].perm_name == "owner" 
-                                                ? payload.permissions[0].required_auth.keys[0].key 
-                                                : payload.permissions[1].required_auth.keys[0].key}
-                                            </Col>
-                                          </FormGroup>
-                                          <FormGroup row>
-                                            <ColBoldStyled sm={2}>Active Public Key:</ColBoldStyled>
-                                            <Col sm={10}>
-                                              {payload.permissions[0].perm_name == "active" 
-                                                ? payload.permissions[0].required_auth.keys[0].key 
-                                                : payload.permissions[1].required_auth.keys[0].key}
-                                            </Col>
-                                          </FormGroup>
-                                        </Form>
-                                      </CardBody>
-                                    </Card>
-                                  </Col>
-                                </Row>
-                                <Row>
-                                  <Col sm="12">
-                                    <CodeViewer
-                                      language="json"
-                                      value={JSON.stringify(payload, null, 2)}
-                                      readOnly={true}
-                                      height={600}
-                                    />
-                                  </Col>  
-                                </Row>        
-                              </div> 
-                        : console.log("No details section") }
-            </div>}                              
+                    </Row>
+                    <Row>
+                      <Col sm="12">
+                        <CodeViewer
+                          language="json"
+                          value={JSON.stringify(payload, null, 2)}
+                          readOnly={true}
+                          height={600}
+                        />
+                      </Col>  
+                    </Row>        
+                  </div> 
+          : console.log("No details section") }                                         
       </div>
     </div>
   );
