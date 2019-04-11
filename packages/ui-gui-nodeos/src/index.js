d7732671589e0c069eeff366616a7149092920f0
Terry Leung <kingsangterry@gmail.com>
2019-04-11 11:02:53 +0800

    UI: Clear localstorage if first time setup is inititated. Clear persisted store if .env.local last timestamp variable is changed.

--- packages/ui-gui-nodeos/src/index.js
+++ packages/ui-gui-nodeos/src/index.js
@@ -7,6 +7,17 @@ import { PersistGate } from 'redux-persist/integration/react'
 import store, { history, persistor } from 'store';
 import App from 'app';
 
+const currentLastTimestamp = process.env.REACT_APP_LAST_FIRST_TIME_SETUP_TIMESTAMP || 1;
+const storedLastTimestamp = localStorage.getItem('lastTimestamp');
+
+//If the current last timestamp from process.env does not match the stored one, it means user has initiated a new first time setup.
+//Hence, clear the whole persisted store.
+if ( currentLastTimestamp !== storedLastTimestamp ){
+  persistor.purge();
+}
+
+localStorage.setItem('lastTimestamp', currentLastTimestamp);
+
 const AppBundle = (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
