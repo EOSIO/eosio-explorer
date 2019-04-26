98f9243dc07199c8275f603dddd30d478caaa304
Jonathan Cardenas <jonathan.cardenas430@gmail.com>
2019-04-26 09:25:32 +0800

    [...] Made loading styled overlay cover the entire screen

--- packages/api-eosio-compiler/docker-eosio-cdt/contracts/byeworld.cpp
+++ packages/api-eosio-compiler/docker-eosio-cdt/contracts/byeworld.cpp
@@ -2,14 +2,14 @@
 #include <eosiolib/print.hpp>
 using namespace eosio;
 
-class [[eosio::contract("tataworld")]] tataworld : public contract {
+class [[eosio::contract("byeworld")]] byeworld : public contract {
   public:
       using contract::contract;
 
       [[eosio::action]]
       void sendmsg(const std::string &msg) {
-         print( "Wilkommen, ", msg );
+         print( "bye, ", msg );
       }
 };
 
-EOSIO_DISPATCH( tataworld, (sendmsg) )
+EOSIO_DISPATCH( byeworld, (sendmsg) )
