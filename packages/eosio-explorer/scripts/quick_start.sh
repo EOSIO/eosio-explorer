559063fffc56a75fd3e953a7ceb79d954d46ea29
Matthew Kelly <matthew.kelly@block.one>
2019-04-29 12:02:24 +0800

    Added condition to quick_start script to check if build folder has not been created. If quick start is being run in production mode and no build folder is found, then always create a new production build.

--- packages/eosio-explorer/scripts/quick_start.sh
+++ packages/eosio-explorer/scripts/quick_start.sh
@@ -15,6 +15,7 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 GREEN='\033[0;32m'
 
+BUILDPATH="$( pwd -P )/.."
 SCRIPTPATH="$( pwd -P )/../.."
 EOSDOCKER="$SCRIPTPATH/docker-eosio-nodeos"
 MONGODOCKER="$SCRIPTPATH/docker-mongodb"
@@ -23,6 +24,7 @@ GUI="$SCRIPTPATH/eosio-explorer"
 ISDEV=false
 ISDELETE=false
 ISFIRSTTIMESETUP=false
+BUILDAPPLICATION=false
 
 for arg in $@
 do
@@ -39,6 +41,16 @@ do
         ;;
   esac
 done
+
+# If either of these conditions are true: 
+#   first-time-setup is being run
+#   user has not added -dev flag but:
+#     has added -d flag
+#     the build folder does not exist
+# Then build with a new timestamp.
+if ( $ISFIRSTTIMESETUP || (!($ISDEV) && ($ISDELETE || [ ! -e $BUILDPATH"/build" ])) ); then
+  BUILDAPPLICATION=true
+fi
 
 echo " "
 echo "=============================="
@@ -121,8 +133,8 @@ do
   fi
 done
 
-# If it is first-time-setup or it is not -dev and -d, build with a new timestamp.
-if ( $ISFIRSTTIMESETUP || (!($ISDEV) && $ISDELETE )); then
+# If the production version of the application needs to be built
+if ( $BUILDAPPLICATION ); then
   # create a static build of application for production
   echo " "
   echo "=============================="
