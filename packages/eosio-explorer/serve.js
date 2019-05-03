5486f94c5ac875123b7f039bc5648e5bdd57ec97
Terry Leung <kingsangterry@gmail.com>
2019-05-03 09:35:25 +0800

    Serve: Comment update.

--- packages/eosio-explorer/serve.js
+++ packages/eosio-explorer/serve.js
@@ -36,7 +36,7 @@ app.listen(PORT, ()=>{
   if(`${process.env.MODE}` !== `development`){
     let url = "http://localhost:" + PORT;
     console.log(`Application is ready on "${url}".`);
-    console.log(`You can now view @eosio-toppings/eosio-explorer in the browser.`);
+    console.log(`You can now view EOSIO Explorer in the browser.`);
     openBrowser(url);
   }
 });
