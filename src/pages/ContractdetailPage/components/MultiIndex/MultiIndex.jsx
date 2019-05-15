959d1a3260bfd776aa23420fd5890e8f53c89886
varshajnagaraja <varsha.nagaraja@block.one>
2019-05-15 10:49:31 +0800

    Adding scope name parameter to fetch multi index table data

--- src/pages/ContractdetailPage/components/MultiIndex/MultiIndex.jsx
+++ src/pages/ContractdetailPage/components/MultiIndex/MultiIndex.jsx
@@ -6,55 +6,106 @@ import { fetchStart, paramsSet } from './MultiIndexReducer';
 
 import { push } from 'connected-react-router'
 
-import { CardBody, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
+import { CardBody, DropdownToggle, DropdownMenu, DropdownItem, Row, Col} from 'reactstrap';
 import styled from 'styled-components';
 import { CodeViewer } from 'components';
 import isObjectEmpty from 'helpers/is-object-empty';
-import { DropdownStyled, CardStyled, CardHeaderStyled, ErrorDivStyled, ErrorButton } from 'styled';
+import { DropdownStyled, CardStyled, CardHeaderStyled, ErrorDivStyled, ErrorButton, InputStyled, ButtonPrimary, ToolTipUncontrolledStyled } from 'styled';
+
+const ToolTipSVG = () => 
+  <svg width="18px" height="18px" viewBox="0 0 18 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">   
+    <g id="Final-Version" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <g id="Artboard" transform="translate(-93.000000, -44.000000)">
+          <g id="Group-15" transform="translate(93.000000, 44.000000)">
+              <circle id="Oval" fill="#667D96" cx="9" cy="9" r="9"></circle>
+              <text id="i" fontFamily="ProximaNova-Semibold, Proxima Nova" fontSize="14" fontWeight="500" fill="#FFFFFF">
+                  <tspan x="7.313" y="14">i</tspan>
+              </text>
+          </g>
+      </g>
+    </g>
+  </svg>
 
 const SelectLabel = styled.label`
   padding-right: 10px;
   margin-top: 10px;
 `
-const DivFlexStyled = styled.div`
+const DropdownDiv = styled.div`
   display: flex;
-  justify-content: flex-end;
-  padding: 0 15px 25px 15px;
 `
 const CustomErrorDiv = styled(ErrorDivStyled)`
   padding: 0;
+`
+const ScopeDivStyled = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+const LabelStyled = styled.div`
+  margin: auto 0;
+`
+const ScopeInputStyled = styled(InputStyled)`
+  width: 30%;
 `
 
 const MultiIndex = (props) => {
 
   const [dropDownSelctedValue, setSelectedValue] = useState("Select Table");
   const [showDetailsSection, setShowDetailsSection ] = useState(false);
-  const [ isOpenDropDown, toggleDropDown] = useState(false);  
+  const [ isOpenDropDown, toggleDropDown] = useState(false);
+  const [ scopeName, setScopeName] = useState(props.abiData.name);  
 
   let { multiIndex: { isFetching, data, params } } = props;
   let { payload, error } = data;
   let { abiData } = props;
-  
+
   return (
     <div className="MultiIndex">
-      <DivFlexStyled>
-        <SelectLabel>Select multi-index table to display data:</SelectLabel>
-        <DropdownStyled isOpen={isOpenDropDown} toggle={()=>{toggleDropDown(!isOpenDropDown)}}>
-          <DropdownToggle caret>{dropDownSelctedValue}</DropdownToggle>
-          <DropdownMenu right>
-            {(abiData.abi.tables).map((eachTable)=>
-                <DropdownItem 
-                  key={eachTable.name} 
-                  onClick={()=>{ 
-                    setSelectedValue(eachTable.name);
-                    props.paramsSet({contract_name: abiData.name, table_name: eachTable.name });
-                    props.fetchStart();
-                    setShowDetailsSection(true);   
-                  }}>
-                {eachTable.name}</DropdownItem>)}
-          </DropdownMenu>     
-        </DropdownStyled>               
-      </DivFlexStyled>                
+      <Row>
+        <Col sm={6}>
+          <DropdownDiv>
+            <SelectLabel>Select multi-index table to display data:</SelectLabel>
+            <DropdownStyled isOpen={isOpenDropDown} toggle={()=>{toggleDropDown(!isOpenDropDown)}}>
+              <DropdownToggle caret>{dropDownSelctedValue}</DropdownToggle>
+              <DropdownMenu right>
+                {(abiData.abi.tables).map((eachTable)=>
+                    <DropdownItem 
+                      key={eachTable.name} 
+                      onClick={()=>{ 
+                        setSelectedValue(eachTable.name);                          
+                      }}>
+                    {eachTable.name}</DropdownItem>)}
+              </DropdownMenu>     
+            </DropdownStyled>  
+          </DropdownDiv>
+        </Col>
+        <Col sm={6}>
+          <ScopeDivStyled>
+            <LabelStyled>Scope Name:&nbsp;&nbsp;</LabelStyled>
+            <LabelStyled id="scopeTooltip"> <ToolTipSVG />&nbsp;&nbsp;&nbsp;</LabelStyled>
+            <ToolTipUncontrolledStyled placement="top" target="scopeTooltip"
+              delay={{ show: 0, hide: 0}}
+              trigger="hover focus"
+              autohide={true}>
+              Scope name default value is set to Smart Contract name, change it accordingly
+              </ToolTipUncontrolledStyled>          
+            <ScopeInputStyled 
+              value={scopeName}
+              onChange = {evt=> {
+                setScopeName(evt.target.value);
+              }} />&nbsp;&nbsp;&nbsp;
+            <ButtonPrimary
+              disabled = {dropDownSelctedValue === "Select Table"}
+              onClick={evt => {
+                props.paramsSet({contract_name: abiData.name, table_name: dropDownSelctedValue, scope_name: scopeName || abiData.name });
+                props.fetchStart();
+                setShowDetailsSection(true);                               
+              }}>
+              Get Data
+            </ButtonPrimary>  
+          </ScopeDivStyled>
+        </Col>
+      </Row>   
+      <br />            
       {error
         ? 
           <>
