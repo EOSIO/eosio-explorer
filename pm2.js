50fad01ead84f475e56c92369326d6f160c734b4
ajay matharu <ajmatharu@gmail.com>
2019-05-15 14:15:49 +0800

    Fogbugz 3398 - Start compiler service using pm2 - Pass compiler service CWD to pm2.js - Display the status of app/compiler after starting

--- pm2.js
+++ pm2.js
@@ -5,11 +5,36 @@ pm2.connect(function(err) {
     console.error(err);
     process.exit(2);
   }
-  
+
   pm2.start({
+    name      : "eosio compiler",
+    script    : 'yarn start',
+    cwd: process.argv.slice(2)[0]
+  }, function(err, apps) {
+    pm2.disconnect();   // Disconnects from PM2
+    if (err) throw err
+  });
+
+  pm2.start({
+    name      : "eosio explorer",
     script    : 'serve.js'         // Script to be run
   }, function(err, apps) {
     pm2.disconnect();   // Disconnects from PM2
     if (err) throw err
   });
+
+  pm2.list((err, apps)=>{
+    if(err)
+      console.log("error encountered -> ", err);
+    else {
+      console.log("\x1b[36mstarting app and compiler using pm2\x1b[0m");
+      apps.forEach(app=>{
+        if (app.pm2_env.status === "online")
+          console.log("\x1b[34m" + app.name + "\x1b[0m ---- \x1b[32m" + app.pm2_env.status + "\x1b[0m");
+        else 
+        console.log("\x1b[34m" + app.name + "\x1b[0m ---- \x1b[31m" + app.pm2_env.status + "\x1b[0m");
+      })
+    }
+  });
+
 });
