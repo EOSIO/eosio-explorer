29a37e9b449930d8a87265bb9774087329f64f89
varshajnagaraja <varsha.nagaraja@block.one>
2019-05-15 15:33:39 +0800

    Fogbugz 3391: Remove the whole Footer

--- src/components/Footer/components/index.js
+++
@@ -1,5 +0,0 @@
-import BoxMiniSocial from './BoxMiniSocial';
-
-export {
-  BoxMiniSocial,
-}
--- src/components/Footer/index.js
+++
@@ -1,7 +0,0 @@
-
-import Loadable from 'react-loadable';
-
-export default Loadable({
-  loader: () => import('./Footer'),
-  loading: () => false
-});
