193733aa54afc2384d9639d1a126814a7ca605ab
Ajay Matharu <ajmatharu@gmail.com>
2019-05-17 18:07:48 +0800

    Pm2 enhancement (#68)
    * Enhance how we display pm2 app status
    
    * code improvements
    - move error above display status

--- pm2.js
+++ pm2.js
@@ -1,20 +1,31 @@
 const pm2 = require('pm2');
 const fs = require('fs');
 
+const displayStatus = (app) => {
+    if (app.pm2_env.status === "online") {
+      console.log("\x1b[34m" + app.pm2_env.name + "\x1b[0m ---- \x1b[32m" + app.pm2_env.status + "\x1b[0m");
+    }
+    else {
+      console.log("\x1b[34m" + app.pm2_env.name + "\x1b[0m ---- \x1b[31m" + app.pm2_env.status + "\x1b[0m");
+      console.log("\x1b[31msomething went wrong please use \"pm2 logs\" to check the logs\x1b[0m")
+    }
+}
+
 pm2.connect(function(err) {
   if (err) {
     console.error(err);
     process.exit(2);
   }
 
+  console.log("\x1b[36mstarting app and compiler using pm2\x1b[0m");
   if (fs.existsSync(process.argv.slice(2)[0])) {
     pm2.start({
       name      : "eosio compiler",
       script    : 'yarn start',
       cwd: process.argv.slice(2)[0]
-    }, function(err, apps) {
-      pm2.disconnect();   // Disconnects from PM2
+    }, function(err, app) {
       if (err) throw err
+      displayStatus(app[0]);
     });
   }
   else
@@ -23,26 +34,8 @@ pm2.connect(function(err) {
   pm2.start({
     name      : "eosio explorer",
     script    : 'serve.js'         // Script to be run
-  }, function(err, apps) {
-    pm2.disconnect();   // Disconnects from PM2
+  }, function(err, app) {
     if (err) throw err
+    displayStatus(app[0]);
   });
-
-  pm2.list((err, apps)=>{
-    if(err)
-      console.log("error encountered -> ", err);
-    else {
-      console.log("\x1b[36mstarting app and compiler using pm2\x1b[0m");
-      apps.forEach(app=>{
-        if (app.pm2_env.status === "online") {
-          console.log("\x1b[34m" + app.name + "\x1b[0m ---- \x1b[32m" + app.pm2_env.status + "\x1b[0m");
-        }
-        else {
-          console.log("\x1b[34m" + app.name + "\x1b[0m ---- \x1b[31m" + app.pm2_env.status + "\x1b[0m");
-          console.log("\x1b[31msomething went wrong please use \"pm2 logs\" to check the logs\x1b[0m")
-        }
-      })
-    }
-  });
-
 });
