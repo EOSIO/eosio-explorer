0e1d2d313bb79cb7e08a79460222c3aac0fe3d20
Terry Leung <kingsangterry@gmail.com>
2019-05-21 10:17:10 +0800

    Change name "Events" to "Console Log" for the logger panel.

--- src/components/ConsoleLogger/ConsoleLogger.jsx
+++ src/components/ConsoleLogger/ConsoleLogger.jsx
@@ -65,7 +65,7 @@ class ConsoleLogger extends Component {
                   ? <i className="fa fa-window-minimize"></i>
                   : <i className="fa fa-window-maximize"></i>
               }
-              <span>Events</span>
+              <span>Console Log</span>
             </div>
             { !!unseen && <span className="badge">{unseen}</span> }
           </div>
