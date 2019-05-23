07813eae7e519ac1dce4eacff9486ddc3df15f30
Matthew Kelly <matthew.kelly@block.one>
2019-05-23 16:14:12 +0800

    Added version number to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 function Footer() {
   return (
     <>
+      <span>v{process.env.REACT_APP_VERSION}</span> 
       <span className="ml-auto"><a target="_blank" rel="noopener noreferrer" href="https://block.one/privacy-policy/">Privacy Policy</a></span>
     </>
   )
