b95f02823d82c47bf33725b88574dd996b303d05
Terry Leung <kingsangterry@gmail.com>
2019-05-23 16:43:59 +0800

    Footer style update. Add github repo url for the version text.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,9 @@ import React from 'react'
 function Footer() {
   return (
     <>
-      <span>v{process.env.REACT_APP_VERSION}</span> 
-      <span className="ml-auto"><a target="_blank" rel="noopener noreferrer" href="https://block.one/privacy-policy/">Privacy Policy</a></span>
+      <a target="_blank" rel="noopener noreferrer" href="https://block.one/privacy-policy/">Privacy Policy</a>
+      <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/EOSIO/eosio-explorer">v{process.env.REACT_APP_VERSION}</a>
     </>
   )
 }
