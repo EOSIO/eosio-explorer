104af9e7df2fde7334dc6c1526ef3ecd9224b488
varshajnagaraja <varshajn99@gmail.com>
2019-05-27 16:42:55 +0800

    Fogbugz 3411: Improper not found styling on Action Detail Page (#100)

--- src/pages/ActiondetailPage/components/Actionjson/Actionjson.jsx
+++ src/pages/ActiondetailPage/components/Actionjson/Actionjson.jsx
@@ -10,17 +10,13 @@ const Actionjson = (props) => {
 
   return (
     <>
-      {payload && (payload.length > 0 ?
-          <CodeViewer
-            language="json"
-            value={JSON.stringify(payload, null, 2)}
-            readOnly={true}
-            height={600} />
-        :
-          <div className="text-center">
-            <p className="text-danger mb-0">Action data empty</p>
-          </div>
-      )}              
+      {
+        <CodeViewer
+          language="json"
+          value={JSON.stringify(payload, null, 2)}
+          readOnly={true}
+          height={600} />
+      }              
     </>
   );
 }
