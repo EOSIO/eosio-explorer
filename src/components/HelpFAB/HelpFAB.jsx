e7fadf7a951444c52a0a64d834023c21f9d1595d
varshajnagaraja <varshajn99@gmail.com>
2019-05-27 17:40:46 +0800

    Fogbugz 3428: Help button not linking to context sensitive help (#103)

--- src/components/HelpFAB/HelpFAB.jsx
+++ src/components/HelpFAB/HelpFAB.jsx
@@ -1,16 +1,54 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 import './HelpFAB.scss';
 
-function HelpFAB() {
+const getPath = (pathname) => pathname.split("/")[1];
+
+const getUrl = (pageName) =>{
+  switch(pageName){
+    case 'block-list':
+      return "//github.com/EOSIO/eosio-explorer/tree/master/docs/pages/block-list-page.md";         
+    case 'block':
+      return "//github.com/EOSIO/eosio-explorer/blob/master/docs/pages/detail-pages/block-detail-page.md";
+    case 'transaction-list':
+      return "//github.com/EOSIO/eosio-explorer/blob/master/docs/pages/transaction-list-page.md";
+    case 'transaction':
+      return "//github.com/EOSIO/eosio-explorer/blob/master/docs/pages/detail-pages/transaction-detail-page.md";  
+    case 'action-list':
+      return "//github.com/EOSIO/eosio-explorer/blob/master/docs/pages/action-list-page.md"; 
+    case 'action':
+      return "//github.com/EOSIO/eosio-explorer/blob/master/docs/pages/detail-pages/action-detail-page.md";
+    case 'account':
+      return "//github.com/EOSIO/eosio-explorer/blob/master/docs/pages/detail-pages/account-detail-page.md";
+    case 'contract':
+      return "//github.com/EOSIO/eosio-explorer/blob/master/docs/pages/detail-pages/smart-contract-detail-page.md";
+    case 'permission':
+      return "//github.com/EOSIO/eosio-explorer/blob/master/docs/pages/interact/manage-accounts-page.md";  
+    case 'deploy':
+      return "//github.com/EOSIO/eosio-explorer/blob/master/docs/pages/interact/deployment-page.md";  
+    case 'push-action':
+      return "//github.com/EOSIO/eosio-explorer/blob/master/docs/pages/interact/push-action-page.md";
+    default: 
+      return "//github.com/EOSIO/eosio-explorer/tree/master/docs";
+  }
+}
+
+const HelpFAB = (props) => {
+
+  const redirectToGithub = (e) => {
+    e.preventDefault();
+    let pageName = getPath(props.location.pathname);
+    window.open(getUrl(pageName), '_blank');
+  }
+
   return (
     <div className="HelpFAB">
-      <Link to="//github.com/EOSIO/eosio-explorer/tree/master/docs" target="_blank" className="btn btn-pill btn-primary fab">
+      <Link to="" onClick={(e) => redirectToGithub(e)} className="btn btn-pill btn-primary fab">
         <i className="material-icons md-fab">help</i>
       </Link>
     </div>
   )
 }
 
-export default HelpFAB;
+export default withRouter(HelpFAB);
