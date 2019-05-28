540f3b7e3403f3b32c1aab53e8ec3f3acf944fa5
Terry Leung <kingsangterry@gmail.com>
2019-05-28 14:26:49 +0800

    Refactor all pollingEpic by using exhaustMap (#106)
    * Refactor all pollingEpic by using exhaustMap to replace manual redux store isFetching check.
    Remove isFetching reducers, fetch_start and fetch_end action of polling reducers.
    
    * remove import store

--- src/reducers/lastblockinfo.js
+++ src/reducers/lastblockinfo.js
@@ -5,10 +5,9 @@
 */
 
 import { combineReducers } from 'redux';
-import { interval, of, empty } from 'rxjs';
-import { switchMap, mergeMap, mapTo, map, takeUntil, catchError, delay, startWith, finalize } from 'rxjs/operators';
+import { interval, of} from 'rxjs';
+import { switchMap, mapTo, map, takeUntil, catchError, delay, startWith, exhaustMap } from 'rxjs/operators';
 import { combineEpics, ofType } from 'redux-observable';
-import store from 'store';
 
 import apiRpc from 'services/api-rpc';
 import { errorLog } from 'helpers/error-logger';
@@ -18,18 +17,14 @@ import { errorLog } from 'helpers/error-logger';
 const actionPrefix = `lastblockinfo/`;
 
 //Action Type
-const FETCH_START = actionPrefix + `FETCH_START`;
 export const FETCH_FULFILLED = actionPrefix + `FETCH_FULFILLED`;
 const FETCH_REJECTED = actionPrefix + `FETCH_REJECTED`;
-const FETCH_END = actionPrefix + `FETCH_END`;
 const POLLING_START = actionPrefix + `POLLING_START`;
 const POLLING_STOP = actionPrefix + `POLLING_STOP`;
 
 //Action Creator
-export const fetchStart = () => ({ type: FETCH_START });
 export const fetchFulfilled = payload => ({ type: FETCH_FULFILLED, payload });
 export const fetchRejected = ( payload, error ) => ({ type: FETCH_REJECTED, payload, error });
-export const fetchEnd = () => ({ type: FETCH_END });
 export const pollingStart = () => ({ type: POLLING_START });
 export const pollingStop = () => ({ type: POLLING_STOP });
 
@@ -40,12 +35,9 @@ const pollingEpic = ( action$, state$ ) => action$.pipe(
   switchMap(action =>
     interval(process.env.REACT_APP_POLLING_INTERVAL_TIME).pipe(
       startWith(-1),
-      mergeMap(index => {
-        let { value: { lastblockinfo: { isFetching } }} = state$;
-
-        return isFetching ? empty() : apiRpc("get_info").pipe(
-          startWith("fetchStart"),
-          map(res => { return res === "fetchStart" ? fetchStart() : fetchFulfilled(res)}),
+      exhaustMap(index => {
+        return apiRpc("get_info").pipe(
+          map(res => fetchFulfilled(res)),
           catchError(error => {
             errorLog("Info page/ get last irreversible block error",error);
             return of(fetchRejected(error.response, { status: error.status }))
@@ -55,10 +47,6 @@ const pollingEpic = ( action$, state$ ) => action$.pipe(
       takeUntil(action$.pipe(
         ofType(POLLING_STOP, POLLING_START, FETCH_REJECTED),
       )),
-
-      finalize(() => {
-        store.dispatch(fetchEnd());
-      })
     )
   ),
 );
@@ -103,21 +91,6 @@ const dataReducer = (state=dataInitState, action) => {
   }
 };
 
-const isFetchingReducer = (state = false, action) => {
-  switch (action.type) {
-    case FETCH_START:
-      return true;
-
-    case FETCH_FULFILLED:
-    case FETCH_REJECTED:
-    case FETCH_END:
-      return false;
-
-    default:
-      return state;
-  }
-};
-
 const isPollingReducer = (state = false, action) => {
   switch (action.type) {
     case POLLING_START:
@@ -134,6 +107,5 @@ const isPollingReducer = (state = false, action) => {
 
 export const combinedReducer = combineReducers({
   data: dataReducer,
-  isFetching: isFetchingReducer,
   isPolling: isPollingReducer
 })
