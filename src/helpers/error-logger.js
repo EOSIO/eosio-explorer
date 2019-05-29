c07095c796611c1e2699d549c57099cab2ab668f
Jonathan Cardenas <jonathan.cardenas430@gmail.com>
2019-05-29 11:34:47 +0800

    Adjustments to in-app console log terminal (#110)
    * Reset the unseen badge indicator on the console log when navigating to a different page from the current view
    * Added a check to only dispatch console log reveal action if there is an actual error object/message

--- src/helpers/error-logger.js
+++ src/helpers/error-logger.js
@@ -2,6 +2,8 @@ import store from 'store';
 import { logStart } from 'reducers/errorlog';
 
 export const errorLog = (...obj) => {
-  store.dispatch(logStart());
-  console.log([...obj, new Date()]);
+  if (obj) {
+    store.dispatch(logStart());
+    console.log([...obj, new Date()]);
+  }
 }
